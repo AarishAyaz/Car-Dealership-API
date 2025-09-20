827b76c49b08ee2767d4ca3f74f15bd00a64f2fe
Aarish Ayaz <aarishk623@gmail.com>
2025-09-20 23:32:46 +0500

    CRUD Completed

--- controllers/car-controller.js
+++ controllers/car-controller.js
@@ -27,7 +27,31 @@ const getAllCars = async (req, res) => {
   }
 };
 
-const getSingleCarbyID = async (req, res) => {};
+const getSingleCarbyID = async (req, res) => {
+  try {
+    const getCurrentCarid = req.params.id;
+    const carDetailsbyId = await Car.findById(getCurrentCarid);
+    if(!getCurrentCarid){
+      res.status(404).json({
+        success:false,
+        message: 'Car not found please try again with different id'
+      })
+      
+    }else{
+      res.status(200).json({
+        success:true,
+        message:'Car found successfully!',
+        data:carDetailsbyId
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success:false,
+      message:'Something went wrong..Please try again!'
+    })
+  }
+};
 
 const addNewCar = async (req, res) => {
   try {
@@ -54,9 +78,61 @@ const addNewCar = async (req, res) => {
   }
 };
 
-const updateCar = async (req, res) => {};
+const updateCar = async (req, res) => {
+  try {
+    const getCurrentcarID = req.params.id;
+    const updatedCarFormData = req.body;
+    const updatedCar = await Car.findByIdAndUpdate(
+      getCurrentcarID,updatedCarFormData,
+      {
+        new:true
+      }
+      )
+    if(!getCurrentcarID){
+      res.status(404).json({
+        success:false,
+        message:'Book not found try again with different id!'
+      })
+    }else{
+      res.status(200).json({
+        success:true,
+        message:"Book found and updated successfully",
+        data:updatedCar
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500),json({
+      success:false,
+      message:'Something went wrong Please try again'
+    })
+  }
+};
 
-const deleteCar = async (req, res) => {};
+const deleteCar = async (req, res) => {
+  try {
+    const getCurrentCarId = req.params.id;
+    const deletedCar = await Car.findByIdAndDelete(getCurrentCarId);
+    if(!getCurrentCarId){
+      res.status(500).json({
+        success:false,
+        message:'Car Not Found please try again with different id'
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: ' Book found and deleted successfully!',
+        data:deletedCar
+      })
+    }
+  } catch (error) {
+     console.log(error);
+    res.status(500),json({
+      success:false,
+      message:'Something went wrong Please try again'
+    })
+  }
+};
 
 module.exports = {
   getAllCars,
